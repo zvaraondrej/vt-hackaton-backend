6327f37d2d4a51442a1c60756e780953e24efdd5
zvaraondrej <zvara.ondrej@gmail.com>
2017-05-18 11:43:31 +0200

    Preparing the vt-hackaton project

--- client/app/app.js
+++ client/app/app.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import createBrowserHistory from 'history/createBrowserHistory';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import configure from './store/configure';
 
-import Navbar from './components/navbar/navbar.component';
-import Footer from './components/footer/footer.component';
-import About from './components/about/about.component';
 import Home from './components/home/home.component';
 
 import './../sass/main.scss';
 
 const store = configure();
-const hist = createBrowserHistory();
 
 render(
   <Provider store={store}>
-    <Router history={hist}>
-      <div>
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Footer />
-      </div>
-    </Router>
+    <Home />
   </Provider>,
   document.getElementById('app'),
 );
---
+++ client/app/components/map/map.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import L from 'leaflet';
+
+export default function Map() {
+  return (
+    <h1>Hello worlds</h1>
+  );
+}
